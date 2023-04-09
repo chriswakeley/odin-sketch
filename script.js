a53efce36c513eb3f09ff4bd62ab1f31c42b2e5e
chriswakeley <chriswakeley@gmail.com>
2023-04-09 15:47:45 -0400

    Finish alpha drawing

---
+++ script.js
@@ -0,0 +1,39 @@
+const DEFAULT_GRID_SIZE = 16;
+const ALPHA_INCREASE = 0.1;
+
+function updateGridCell(event){
+    const value = getComputedStyle(this).getPropertyValue("background-color");
+    
+    // Get all color components (alpha may not be there if = 1):
+    const parts = value.match(/[\d.]+/g);
+    
+    // If alpha is not there, add it:
+    if (parts.length === 3) {
+      parts.push(1);
+    }
+    
+    // Modify alpha:
+    parts[3] = Math.min(1, Math.max(0, parseFloat(parts[3]) + ALPHA_INCREASE));
+    
+    // Apply new value:
+    this.style.backgroundColor = `rgba(${ parts.join(',') })`;
+}
+
+function makeGrid(gridSize) {
+    const canvasContainer = document.querySelector(".canvas-container");
+    const gridRows = [];
+    for (let i = 0; i < gridSize; i++) {
+        const gridRow = document.createElement('div');
+        gridRow.classList.add("grid-row");
+        for (let y = 0; y < gridSize; y++) {
+            const gridCell = document.createElement('div');
+            gridCell.classList.add("grid-cell");
+            gridCell.addEventListener("mouseover", updateGridCell);
+            gridRow.appendChild(gridCell);
+        }
+        gridRows.push(gridRow);
+    }
+    canvasContainer.replaceChildren(...gridRows);
+}
+
+makeGrid(DEFAULT_GRID_SIZE);
