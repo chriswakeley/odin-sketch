fcde7c51b731d94447606ee8be27fc21195f978e
chriswakeley <chriswakeley@gmail.com>
2023-04-10 13:09:18 -0400

    Change Rainbow to continuous hue

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const DEFAULT_GRID_SIZE = 16;
 const ALPHA_INCREASE = 0.1;
+const RAINBOW_INCREASE = 7;
+
+const HSLToRGB = (h, s, l) => {
+    s /= 100;
+    l /= 100;
+    const k = n => (n + h / 30) % 12;
+    const a = s * Math.min(l, 1 - l);
+    const f = n =>
+        l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+    return [255 * f(0), 255 * f(8), 255 * f(4)];
+};
+
+function generateNextRGBARainbow() {
+    let nextHue = (+(document.querySelector(".canvas-container").dataset.lastHue) + RAINBOW_INCREASE)%360;
+    document.querySelector(".canvas-container").dataset.lastHue = nextHue;
+    const randomRGB = HSLToRGB(nextHue, 100, 50);
+    randomRGB.push(1);
+    return randomRGB;
+}
 
 function updateGridCell(event) {
 
-    let colorToAdd = document.querySelector(".rainbow-brush input").checked ?
-                        [Math.random()*255, Math.random()*255, Math.random()*255, 1] : [0, 0, 0, 1];
+    let colorToAdd = (document.querySelector(".rainbow-brush input").checked) ?
+        generateNextRGBARainbow() :
+        [0, 0, 0, 1];
 
     if (document.querySelector(".soft-brush input").checked) {
 
@@ -21,15 +41,16 @@ function updateGridCell(event) {
         // Modify alpha:
         parts[3] = Math.min(1, Math.max(0, parseFloat(parts[3]) + ALPHA_INCREASE));
 
-        if(document.querySelector(".rainbow-brush input").checked){
-            for(let i = 0; i < 3; i++){
-                parts[i] = Math.min(255, Math.max(0, parseFloat(parts[i]) + colorToAdd[i]*ALPHA_INCREASE));
+        //Blend color with random rainbow color
+        if (document.querySelector(".rainbow-brush input").checked) {
+            for (let i = 0; i < 3; i++) {
+                parts[i] = Math.min(255, Math.max(0, parseFloat(parts[i]) + colorToAdd[i] * ALPHA_INCREASE));
             }
         }
-        else{
-        //Modify color towards black
-            for(let i = 0; i < 3; i++){
-                parts[i] = Math.min(255, Math.max(0, parseFloat(parts[i]) - 255*ALPHA_INCREASE));
+        else {
+            //Modify color towards black
+            for (let i = 0; i < 3; i++) {
+                parts[i] = Math.min(255, Math.max(0, parseFloat(parts[i]) - 255 * ALPHA_INCREASE));
             }
         }
 
@@ -58,12 +79,12 @@ function makeGrid(gridSize) {
     canvasContainer.replaceChildren(...gridRows);
 }
 
-function handleNewGrid(){
+function handleNewGrid() {
     let newGridSize = prompt("Please enter a new grid size, 1-100");
-    if(newGridSize === null){
+    if (newGridSize === null) {
         alert("New Grid Cancelled");
     }
-    else if(+newGridSize < 1 || +newGridSize > 100 || +newGridSize%1 !== 0){
+    else if (+newGridSize < 1 || +newGridSize > 100 || +newGridSize % 1 !== 0) {
         alert("New Grid Cancelled, not a valid grid size, integer 1-100");
     }
     else {
