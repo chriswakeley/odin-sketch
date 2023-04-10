601cbaa50aa2fe045c1e22da201476ea5d1a1630
chriswakeley <chriswakeley@gmail.com>
2023-04-09 20:13:50 -0400

    Add soft and rainbow brush functionality

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 const DEFAULT_GRID_SIZE = 16;
 const ALPHA_INCREASE = 0.1;
 
-function updateGridCell(event){
-    const value = getComputedStyle(this).getPropertyValue("background-color");
-    
-    // Get all color components (alpha may not be there if = 1):
-    const parts = value.match(/[\d.]+/g);
-    
-    // If alpha is not there, add it:
-    if (parts.length === 3) {
-      parts.push(1);
+function updateGridCell(event) {
+
+    let colorToAdd = document.querySelector(".rainbow-brush input").checked ?
+                        [Math.random()*255, Math.random()*255, Math.random()*255, 1] : [0, 0, 0, 1];
+
+    if (document.querySelector(".soft-brush input").checked) {
+
+        const value = getComputedStyle(this).getPropertyValue("background-color");
+
+        // Get all color components (alpha may not be there if = 1):
+        const parts = value.match(/[\d.]+/g);
+
+        // If alpha is not there, add it:
+        if (parts.length === 3) {
+            parts.push(1);
+        }
+
+        // Modify alpha:
+        parts[3] = Math.min(1, Math.max(0, parseFloat(parts[3]) + ALPHA_INCREASE));
+
+        if(document.querySelector(".rainbow-brush input").checked){
+            for(let i = 0; i < 3; i++){
+                parts[i] = Math.min(255, Math.max(0, parseFloat(parts[i]) + colorToAdd[i]*ALPHA_INCREASE));
+            }
+        }
+        else{
+        //Modify color towards black
+            for(let i = 0; i < 3; i++){
+                parts[i] = Math.min(255, Math.max(0, parseFloat(parts[i]) - 255*ALPHA_INCREASE));
+            }
+        }
+
+        // Apply new value:
+        this.style.backgroundColor = `rgba(${parts.join(',')})`;
     }
-    
-    // Modify alpha:
-    parts[3] = Math.min(1, Math.max(0, parseFloat(parts[3]) + ALPHA_INCREASE));
-    
-    // Apply new value:
-    this.style.backgroundColor = `rgba(${ parts.join(',') })`;
+    else {
+        this.style.backgroundColor = `rgba(${colorToAdd.join(',')})`;
+    }
 }
 
 function makeGrid(gridSize) {
